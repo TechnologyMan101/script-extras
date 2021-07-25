d4f9ac5af32a948fd4db275476c84c5fd79fe8f0
NP Viroonc <npviro@gmail.com>
2021-07-24 19:09:46 -0700

    Update Set VM Screen to 1920x1080.sh

--- Set VM Screen to 1920x1080.sh
+++ Set VM Screen to 1920x1080.sh
@@ -1,27 +1,59 @@
 #!/bin/bash
 # Start of Function Cluster
+checkcompatibility () {
+	if ! echo $XDG_SESSION_TYPE | grep -qi "x11"
+	then
+		sysreqfail
+	fi
+}
+sysreqfail () {
+	clear
+	tput setaf 9
+	echo "System requirements not met. This script supports systems logged in with Xorg!!!"
+	tput setaf 10
+	echo "Current Session Type: $XDG_SESSION_TYPE"
+	tput sgr0
+	echo "Hit any key to exit:"
+	IFS=""
+	read -sN1 answer
+	quitscript
+}
 mainmenu () {
 	clear
 	tput setaf 3
 	echo "================================="
 	echo " --- Set Screen to 1920x1080 ---"
 	echo "================================="
+	tput setaf 10
+	echo "Current Session Type: $XDG_SESSION_TYPE"
 	tput setaf 9
-	echo "This should only be used on a VM!!!"
+	echo "This should only be used on a VM unless your actual computer refuses to set your desired resolution!!!"
+	echo "This script will only work on Xorg!!!"
 	tput setaf 10
 	echo "This script can be modified to set a different resolution."
 	tput setaf 3
 	echo "Hit <return> to run this script."
 	tput setaf 9
-	echo "Press Q followed by <return> to quit."
+	echo "Press Q to quit."
 	tput sgr0
-	echo "Enter your selection followed by <return>:"
-	read answer
-	case "$answer" in
-		q) quitscript;;
-		Q) quitscript;;
+	echo "Enter your selection:"
+	IFS=""
+	read -sN1 answer
+	case $(echo "$answer" | tr A-Z a-z) in
+		"")	runscript;;
+		q)	quitscript;;
+		*)	badoption;;
 	esac
-	runscript
+}
+badoption () {
+	clear
+	tput setaf 9
+	echo "Invalid Option!"
+	tput setaf 3
+	echo "Returning to Main Menu..."
+	tput sgr0
+	sleep 3
+	mainmenu
 }
 quitscript () {
 	tput sgr0
@@ -55,6 +87,7 @@ runscript () {
 # Start of Main Script
 while true
 do
+	checkcompatibility
 	mainmenu
 done
 # End of Main Script
